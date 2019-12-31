90979e5bb67a0cbdca3527e646a1aaec4a16194f
Luis Andres Villegas <luis.villegas@unisimon.edu.co>
2019-12-31 10:39:51 -0500

    cambios de algunas clases a HOOKS

--- src/pages/BadgeDetailsContainer.js
+++ src/pages/BadgeDetailsContainer.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 
 
 import Loading from './Loading';
@@ -6,70 +6,69 @@ import api from '../api';
 import Error from './Error';
 import BadgeDetails from './BadgeDetails';
 
-class BadgeDetailsContainer extends React.Component{
+function BadgeDetailsContainer(props) {
 
-    state = {
-        loading: true,
-        error: null,
-        data: undefined,
-        modalIsOpen: false
-    };
+    const [isOpen, setIsOpen] = useState(false);
 
-    componentDidMount(){
-        this.fetchData();
-    }
+    const [state, setState] = useState(
+        {
+            loading: true,
+            error: null,
+            data: undefined
+        });
 
-    fetchData = async e => {
-        
-        this.setState({loading: true, error: null});
+    useEffect(() => {
 
-        try{
-            const data = await api.badges.read(this.props.match.params.badgeId);
-            this.setState({loading: false, data: data});
-        }catch(error){
-            this.setState({loading: false, error});
+        if (state.loading)
+            fetchData();
+    })
+
+    async function fetchData() {
+        try {
+            const data = await api.badges.read(props.match.params.badgeId);
+            setState({ loading: false, data });
+        } catch (error) {
+            setState({ loading: false, error });
         }
     }
 
-    
-    handleOpenModal = e => {
-        this.setState({ modalIsOpen: true })
+    function handleOpenModal(e) {
+        setIsOpen(true)
     }
 
-    handleCloseModal = e => {
-        this.setState({ modalIsOpen: false })
+    function handleCloseModal() {
+        setIsOpen(false)
     }
 
-    handleDeleteBadge = async e =>{
-        this.setState({loading: true, error: null})
+    async function handleDeleteBadge(e) {
 
-        try{
+        try {
 
-            await api.badges.remove(this.props.match.params.badgeId);
-            this.setState({loading: false, error: null})
-            this.props.history.push('/badges');
-
-        }catch(error){
-            this.setState({loading: false, error});
+            console.log('eliminado')
+            await api.badges.remove(props.match.params.badgeId);
+            props.history.push('/badges');
+        } catch (error) {
+            setState({ loading: false, error });
         }
     }
 
-    render(){
-        const {loading, data, error, modalIsOpen} = this.state;
-       
-        if(loading)
-            return <Loading />
+    const { loading, data, error } = state;
 
-        if(error)
-            return <Error type="500"  errorText={error.message} />
+    if (loading)
+        return <Loading />
 
-        return <BadgeDetails 
-                    onCloseModal={this.handleCloseModal} 
-                    onOpenModal={this.handleOpenModal} 
-                    onDeleteBadge={this.handleDeleteBadge}
-                    modalIsOpen={modalIsOpen}
-                    badge={data} />;
-    }
+    if (error)
+        return <Error type="500" errorText={error.message} />
+
+    return (
+        <BadgeDetails
+            onCloseModal={handleCloseModal}
+            onOpenModal={handleOpenModal}
+            onDeleteBadge={handleDeleteBadge}
+            modalIsOpen={isOpen}
+            badge={data}
+        />
+    );
 }
 
 export default BadgeDetailsContainer;
--- src/pages/Badges.js
+++ src/pages/Badges.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { Link } from 'react-router-dom';
 
@@ -8,89 +8,63 @@ import BadgesList from '../components/BadgesList';
 import Loading from '../pages/Loading';
 import Error from '../pages/Error';
 import api from '../api';
-//MONTAJE
-//representa el momento donde se inserta el código del componente en el DOM
-//se llaman tres métodos en este ORDEN: constructor, render, componentDidMout
-//---------
-//Actualizacion
-//ocurre cuando los PROPS o el ESTADO del componente cambia
-//Se llaman dos metodos en este ORDEN: render, componentDidUpdate
-//---------
-//Desmontaje
-//Nos da la importunidad de hacer limpienza de nuestros componentes
-//Se llama un método: ComponentWillUnmount()
-class Badges extends React.Component {
-
-    state = {
-        loading: true,
-        error: null,
-        data: undefined
-    }
 
 
-    componentDidMount() { //Este es el mejor lugar para comenzar una peticion.
-        //peticion GET
-        //es cyakqyuer kkanada a la que se soliciten datos.
-        //peticion POST
-        //Es cuando se envian datos.
-        //PUT
-        //Cuando se modifican datos.
-        //DELETE
-        //cuando se eliminan datos
-        this.fechtData();
+function Badges() {
 
-        //this.interval = setInterval(this.fechtData, 5000);
-    }
+    const [state, setState] = useState(
+        {
+            loading: true,
+            error: null,
+            data: undefined
+        })
 
-    componentWillUnmount(){
-        //clearInterval(this.interval);
-    }
-
-    fechtData = async () => {
-        this.setState({ loading: true, error: null });
-
-        try {
-            const data = await api.badges.list();
-            this.setState({ loading: false, data });
-        } catch (error) {
-            this.setState({ loading: false, error });
-        }
-    }
-
-    render() {
-
-        const {loading, error, data} = this.state;
-
-        if (loading && !data) 
-            return <Loading />;
-    
-        if (error) 
-            return <Error type="500" errorText={this.state.error.message}/>;
+    useEffect(() => {
         
-        return ( //para quitar los DIV usamos React.Fragment
-            <React.Fragment>
-                <div className="Badges">
-                    <div className="Badges-container">
-                        <img className="Badges-conf-logo" src={confLogo} alt="confLogo" />
+        const fetchData = async () =>{
+            try{
+                const data = await api.badges.list();
+                setState({loading: false, data});
+            }catch(error){
+                setState({loading: false, error});
+            }
+        }
+
+        if(state.loading)
+            fetchData();
+    })
+
+    const { loading, error, data } = state;
+
+    if (loading && !data)
+        return <Loading />;
+
+    if (error)
+        return <Error type="500" errorText={error.message} />;
+
+    return (
+        <React.Fragment>
+            <div className="Badges">
+                <div className="Badges-container">
+                    <img className="Badges-conf-logo" src={confLogo} alt="confLogo" />
+                </div>
+            </div>
+
+            {!(data.length === 0) && (
+                <div className="Badges-container">
+                    <div className="Badges-buttons">
+                        <Link className="btn btn-primary" to="/badges/new">New Badge</Link>
                     </div>
                 </div>
+            )}
 
-                {!(data.length === 0) && (
-                    <div className="Badges-container">
-                        <div className="Badges-buttons">
-                            <Link className="btn btn-primary" to="/badges/new">New Badge</Link>
-                        </div>
-                    </div>
-                )}
-
-                <div className="Badges-list">
-                    <div className="Badges-container">
-                        <BadgesList badges={this.state.data} />
-                    </div>
+            <div className="Badges-list">
+                <div className="Badges-container">
+                    <BadgesList badges={data} />
                 </div>
-            </React.Fragment>
-        );
-    }
+            </div>
+        </React.Fragment>
+    );
 }
 
 export default Badges;  
