b3a298daf08638c9ca8b00e3593536047739f354
Luis Andres Villegas <luis.villegas@unisimon.edu.co>
2019-12-31 21:19:22 -0500

    Updates

---
+++ precache-manifest.690682ab96f9520094a092f40b4a6f95.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "c332389463d8e083e78d",
+    "url": "/PlatziBadge/static/css/main.f1387fc5.chunk.css"
+  },
+  {
+    "revision": "c332389463d8e083e78d",
+    "url": "/PlatziBadge/static/js/main.c3323894.chunk.js"
+  },
+  {
+    "revision": "b5d0dfd265d54d4fd7b6",
+    "url": "/PlatziBadge/static/js/runtime~main.b5d0dfd2.js"
+  },
+  {
+    "revision": "adfe58df5497085db22b",
+    "url": "/PlatziBadge/static/css/2.285cacbc.chunk.css"
+  },
+  {
+    "revision": "adfe58df5497085db22b",
+    "url": "/PlatziBadge/static/js/2.adfe58df.chunk.js"
+  },
+  {
+    "revision": "f246f5f1fb702016606ee8c65e33c029",
+    "url": "/PlatziBadge/static/media/platziconf-logo.f246f5f1.svg"
+  },
+  {
+    "revision": "973f5842f0c37eacb2eb491c62fc0032",
+    "url": "/PlatziBadge/static/media/badge-header.973f5842.svg"
+  },
+  {
+    "revision": "1d17360a685ac86023b90aa14fd2c62d",
+    "url": "/PlatziBadge/static/media/stars.1d17360a.svg"
+  },
+  {
+    "revision": "0026b862f1b05c3e27d471ba85c6ebc5",
+    "url": "/PlatziBadge/static/media/logo.0026b862.svg"
+  },
+  {
+    "revision": "71addf454967249472471f5398055e3d",
+    "url": "/PlatziBadge/static/media/astronauts.71addf45.svg"
+  },
+  {
+    "revision": "a6730ac9c1200939e864f8a5a016e3e1",
+    "url": "/PlatziBadge/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/PlatziBadge/precache-manifest.690682ab96f9520094a092f40b4a6f95.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/PlatziBadge/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
